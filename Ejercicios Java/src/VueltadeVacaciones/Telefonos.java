f466c3b65d88ff120c22c1929eed8446b6af57a1
Alejandro Miralles Ramos <alemirram3@alu.edu.gva.es>
2025-01-05 18:18:23 +0100

    Los dos telefonos de VueltadeVacaciones

---
+++ Ejercicios Java/src/VueltadeVacaciones/Telefonos.java
@@ -0,0 +1,19 @@
+package VueltadeVacaciones;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Telefonos {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        Map<String, String>guia=new HashMap<>();
+        guia.put("Alejandro","233-432");
+        guia.put("Maria","543-546");
+        guia.put("Juan","432-434");
+
+        System.out.println("Dime un contacto");
+        String contacto=sc.nextLine();
+        System.out.println(guia.getOrDefault(contacto,"No existe el contacto"));
+    }
+}
