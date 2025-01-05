c6699ea0aa980f29031334b06d56b3557b8f5b12
Alejandro Miralles Ramos <alemirram3@alu.edu.gva.es>
2025-01-05 20:05:17 +0100

    Frecuency y Terminacion de VueltadeVacaciones

---
+++ Ejercicios Java/src/VueltadeVacaciones/Terminacion.java
@@ -0,0 +1,26 @@
+package VueltadeVacaciones;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Terminacion {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        ArrayList<String>Boletos=new ArrayList<>();
+        Boletos.add("00004");
+        Boletos.add("08437");
+        Boletos.add("39804");
+        BoletosRepetidos(Boletos);
+
+    }
+    public static void BoletosRepetidos(ArrayList<String>Boletos){
+        Map<Integer,Integer>boletosrep=new HashMap<>();
+        for(String boleto:Boletos){
+            int terminacion=Character.getNumericValue(boleto.charAt(boleto.length()-1));
+            boletosrep.put(terminacion,boletosrep.getOrDefault(terminacion,0)+1);
+        }
+        System.out.println(boletosrep);
+    }
+}
