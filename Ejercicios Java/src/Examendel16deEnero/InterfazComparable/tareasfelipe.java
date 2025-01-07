3ca4408db4bada4b9d73ba3e7e2e07b238f6f1ee
Alecraft57 <alemirram3@alu.edu.gva.es>
2025-01-07 20:23:41 +0100

    Felipe Hijo de puta

---
+++ Ejercicios Java/src/Examendel16deEnero/InterfazComparable/tareasfelipe.java
@@ -0,0 +1,21 @@
+package Examendel16deEnero.InterfazComparable;
+
+public class tareasfelipe {
+        private int prioridad;
+        private int duracion;
+
+        public tareasfelipe(int prioridad, int duracion) {
+            this.prioridad = prioridad;
+            this.duracion = duracion;
+        }
+
+        public int getPrioridad() {
+            return this.prioridad;
+        }
+
+        public int getDuracion() {
+            return this.duracion;
+        }
+
+    }
+
