adbfb9c1cb86f2499d36675f2fd1dfe7b13e82fb
Alecraft57 <alemirram3@alu.edu.gva.es>
2025-02-26 20:36:15 +0100

    Creacion del ejercicio de SQL x Programacion

---
+++ Ejercicios Java/src/Conexion/Ejercicios/main.java
@@ -0,0 +1,13 @@
+package Conexion.Ejercicios;
+
+public class main {
+    public static void main(String[] args) {
+        poble pb=new poble(20,"Benicarlo",1);
+//        pb.insertar();
+            pb.eliminar();
+//        client cl=new client(230,"Juan","Calle Mislata",12324,20);
+////        cl.insertar();
+//        cl.eliminar();
+        System.out.println();
+    }
+}
