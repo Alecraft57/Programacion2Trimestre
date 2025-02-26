9420a2cc44056f39964cc2972b1037658bf4c670
Alejandro Miralles Ramos <alemirram3@alu.edu.gva.es>
2025-02-26 10:24:20 +0100

    Empleados tablas(creacion, modificacion, insertacion) y carpetas nuevas

--- Ejercicios Java/src/Conexion/Empleados/CreacionEmpleados.java
+++ Ejercicios Java/src/Conexion/Empleados/CreacionEmpleados.java
@@ -6,15 +6,30 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class CreacionEmpleados {
-    public static void main(String[] args) {
-        String createEmpleados = "Create table Empleados (" +" num integer primary key, "+" Nombre varchar(255),"+ " Departamento integer,"+" edad integer,"+" sueldo integer);";
-            try (
-            Connection conn = DatabaseConnection.getConnection();
-            Statement stmt = conn.createStatement();
-            stmt.executeUpdate(createEmpleados){
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+    static java.sql.Connection conn=null;
+    public static void main(String[] args) throws SQLException {
+        Statement stmt=null;
+        String createEmpleados = "Create table Empleados (" +
+                                    " num integer primary key, "+
+                                    " Nombre varchar(255),"+
+                                    " Departamento integer,"+
+                                    " edad integer,"+
+                                    " sueldo integer);";
+            try {
+            conn = DatabaseConnection.getConnection();
+            stmt = conn.createStatement();
+            stmt.executeUpdate(createEmpleados);
+        } catch (SQLException ex) {
+                System.out.println("Error "+ ex.getMessage());
+        }finally{
+                try{
+                    if(stmt != null && !stmt.isClosed()){
+                        stmt.close();
+                    }
+                }catch (SQLException ex){
+                    System.out.println("No se ha podido cerrar el Statement por alguna razon"+ ex.getMessage());
+                }
+            }
         }
     }
 
