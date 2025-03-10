5e8b713e5bee90ec849dc5a67914c0ca8ecf6969
Alecraft57 <alemirram3@alu.edu.gva.es>
2025-03-10 20:39:20 +0100

    Nueva Base de Datos

---
+++ Ejercicios Java/src/Conexion/BasesLocal/Eventos.java
@@ -0,0 +1,48 @@
+package Conexion.BasesLocal;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class Eventos {
+    private int eventos;
+    private String nombre_evento;
+    private int fecha;
+    private int hora;
+
+    public void insertar(int id_eventos,String nombre_evento,String fecha, int hora){
+        String url="jdbc:sqlite:Conciertos.db";
+        Connection con=null;
+        PreparedStatement st=null;
+        String sql="insert into Eventos (id_eventos,nombre_evento,fecha,hora) values(?,?,?,?)";
+        try {
+            con= DriverManager.getConnection(url);
+            System.out.println("La conexion ha sido exitosa");
+            st=con.prepareStatement(sql);
+
+            st.setInt(1,id_eventos);
+            st.setString(2,nombre_evento);
+            st.setString(3,fecha);
+            st.setInt(4,hora);
+            st.executeUpdate();
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            if(con!=null && con.isClosed()){
+                con.close();
+                System.out.println("Conexion cerrada");
+            }
+        }catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        try {
+            if(st!=null && st.isClosed()){
+                st.close();
+                System.out.println("Conexion cerrada");
+            }
+        }catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+}
---
+++ Ejercicios Java/src/Conexion/BasesLocal/main.java
@@ -0,0 +1,8 @@
+package Conexion.BasesLocal;
+
+public class main {
+    public static void main(String[] args) {
+        Eventos e = new Eventos();
+        e.insertar(20,"Papillas","20/5/2025",20);
+    }
+}
