745c7b5ba3c1ef78dd24b7934459c3d5e804c845
alemirram3 <alemirram3@alu.edu.gva.es>
2025-04-03 19:57:57 +0200

    Ejercicio2 actualizado

--- Ejercicios Java/src/Archivos/Gestion/Ejercicio2.java
+++ Ejercicios Java/src/Archivos/Gestion/Ejercicio2.java
@@ -14,7 +14,7 @@ public class Ejercicio2 {
 
         while (true) {
             imprimirDirectorio(directorio);
-            System.out.print("Introduce una opción (-1 para salir\n-2 para cambiar permisos): ");
+            System.out.print("Introduce una opción \n0 Acceder\n-1 Para salir\n-2 Para cambiar permisos\n-3 Contar por extension\n-4 Mostrar ruta del archivo:\n-5 Crear directorio\n-6 Borrar archivo/directorio\n ");
 
             int opcion = Integer.parseInt(reader.readLine());
 
@@ -41,6 +41,45 @@ public class Ejercicio2 {
                     elegido.setReadable(false);
                 } else if (!per.equals("x")){
                     elegido.setReadable(true);
+                }
+            }else if(opcion==-3){
+                File[] files = directorio.listFiles();
+                System.out.println("Dime que extension quieres contar");
+                String res= reader.readLine();
+                int cant_ext=0;
+                for(int i=0;i<files.length;i++){
+                    if(files[i].getName().endsWith(res)){
+                        cant_ext++;
+                    }
+                }
+                System.out.println("Hay "+cant_ext+ " archivos con esta extension");
+
+            }else if(opcion == -4){
+                File[] files = directorio.listFiles();
+                System.out.println("Dime que archivo quieres ver");
+                String res= reader.readLine();
+                for(int i=0;i<files.length;i++){
+                    if(files[i].getName().equals(res)){
+                        System.out.println(files[i].getAbsolutePath());
+                    }
+                }
+            }else if(opcion == -5){
+                System.out.println("Dime el nombre del directorio que quieres crear");
+                String res= reader.readLine();
+                File nuevodirectorio=new File(directorio,res);
+                if(nuevodirectorio.mkdir()){
+                    System.out.println("Directorio "+res+" creado correctamente");
+                }else{
+                    System.out.println("Error al crear el directorio");
+                }
+            }else if(opcion == -6){
+                System.out.println("Dime el nombre del directorio que quieres eliminar");
+                String res= reader.readLine();
+                File nuevodirectorio=new File(directorio,res);
+                if(nuevodirectorio.delete()){
+                    System.out.println("Directorio o archivo" +res+" eliminado correctamente");
+                }else{
+                    System.out.println("Error al eliminar el directorio/archivo");
                 }
             }else if (opcion == 0) {
                 directorio = directorio.getParentFile();
@@ -101,7 +140,7 @@ public class Ejercicio2 {
                 } else {
                     execute = "-";
                 }
-                System.out.println(num + "." + e.getName() + " " + e.length() + "\t" + formatter.format(e.lastModified()) + " " + read + write + execute);
+                System.out.println(num + "." + e.getName() + "<Archivo> " + e.length() + "\t" + formatter.format(e.lastModified()) + " " + read + write + execute);
             }
         }
         if (e.isDirectory()) {
